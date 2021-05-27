67348383b663431e6a7d57ebbc9dbfd168b35c8d
Saul-Ponce <saulponce1698@hotmail.com>
2021-05-26 19:05:32 -0600

    insertar

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const express = require('express')
 const fs = require('fs')
 
 const db = require('./db/db')
+const Telefono = require('./models/Telefono')
+const Usuario = require('./models/Usuario')
 require('./db/relaciones')
 const app = express()
 const port = process.env.PORT || 4000
 app.use(express.static('public'))
 app.get('/test', (req, res) => {
     res.end(process.env.HOLA)
+})
+app.get('/insertar-usuario/:nombre/:clave/:telefono', async (req, res) => {
+    const { nombre, clave, telefono } = req.params
+    try {
+        const usuario = await Usuario.create({
+            nombre,
+            clave,
+            telefono: [{ Telefono: telefono }]
+        }, { include: { model: Telefono, as: 'telefono' } })
+        res.json({ usuario })
+    } catch (error) {
+        fs.appendFileSync('log.txt', `${error} \n \n`)
+        console.log(error);
+        res.json({ error })
+    }
+
 })
 app.listen(port, async () => {
     try {
