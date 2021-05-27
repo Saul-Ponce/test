f252cff4f673538b74d098ebe12e168482a057f9
Saul-Ponce <saulponce1698@hotmail.com>
2021-05-26 21:22:33 -0600

    port

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize')
 const db = new Sequelize(process.env.BD, process.env.USUARIO, process.env.PASSWORD, {
     dialect: process.env.DIALECTO,
-    host: process.env.HOST
+    host: process.env.HOST,
+    port: process.env.DBPORT
 })
 module.exports = db
